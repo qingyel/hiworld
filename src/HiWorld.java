cbf4dd84911a9defe06c546d133dbc7ff7526d6b
syx <syx_xd@163.com>
2019-07-26 11:38:08 +0800

    提交代码

---
+++ src/HiWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @program: hiworld
+ * @description:
+ * @author: syx
+ * @create: 2019-07-26 11:35
+ **/
+public class HiWorld {
+    public static void main(String args[]){
+        System.out.println("Hi World");
+    }
+}
